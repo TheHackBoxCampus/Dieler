d113d53c4f736ff82ddd0db24de75b266a335f50
TheHackBoxCampus <millercampusdeveloper@gmail.com>
2024-07-31 07:02:28 -0500

    feat: Componentes, modificaciones y nuevas paginas

--- src/components/ui/custom/carouselEquipment.tsx
+++ src/components/ui/custom/carouselEquipment.tsx
@@ -11,10 +11,14 @@ export const CarouselEquipment = () => {
     <Carousel className="w-[60%] max-h-64">
       <CarouselContent className="w-full h-full flex justify-start gap-5">
         <CarouselItem className="flex items-center h-64 rounded-3xl justify-center">
-          <span className="text-3xl font-bold border border-white w-3/4 h-full text-center rounded-3xl leading-[250px]">1</span>
+          <span className="text-3xl font-bold border border-white w-3/4 h-full text-center rounded-3xl leading-[250px]">
+            1
+          </span>
         </CarouselItem>
         <CarouselItem className="flex items-center h-64 rounded-3xl justify-center">
-        <span className="text-3xl font-bold border border-white w-3/4 h-full text-center rounded-3xl leading-[250px]">2</span>
+          <span className="text-3xl font-bold border border-white w-3/4 h-full text-center rounded-3xl leading-[250px]">
+            2
+          </span>
         </CarouselItem>
       </CarouselContent>
       <CarouselPrevious />
